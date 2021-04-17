0604f87d5e25efc05dd5843db2c6bc4442ceafe0
vmik7 <vmik7@yandex.ru>
2021-04-17 16:18:21 +0300

    Валидация формы

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // Поля для поиска
-let searchInputs = document.querySelectorAll('.input_entry');
+const searchInputs = document.querySelectorAll('.input_entry');
 
 // Навешиваем отправку формы по нажатию Enter на любое поле
 for (let input of searchInputs) {
@@ -23,7 +23,7 @@ for (let input of searchInputs) {
 }
 
 // Кнопки добавления записей в таблицы
-let addButtons = document.querySelectorAll('.button_action_add');
+const addButtons = document.querySelectorAll('.button_action_add');
 
 // Навешиваем отправку формы по нажатию
 for (let btn of addButtons) {
@@ -37,4 +37,47 @@ for (let btn of addButtons) {
             document.body.append(form);
             form.submit();
     });
+}
+
+// Форма добавления записи в БД
+const addForm = document.querySelector('.form_action_add-row');
+
+// Если форма есть на странице - навешиваем обработчик и валидацию
+if (addForm) {
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (validateForm(addForm)) {
+            addForm.submit();
+        }
+    });
+}
+
+// Валидация некоторых полей
+function validateForm(form) {
+    const inputs = form.querySelectorAll('.input');
+    for (let input of inputs) {
+        if (input.classList.contains('input_validate_tel')) {
+            let regExp = new RegExp(/^\+7[0-9]{10}$/);
+            if (!regExp.test(input.value)) {
+                alert("Неправильно задан номер!");
+                return false;
+            }
+        }
+        if (input.classList.contains('input_validate_posnum')) {
+            if (+input.value < 0) {
+                alert("Дисциплин не может быть отрицательное число!");
+                return false;
+            }
+        }
+        if (input.classList.contains('input_validate_char')) {
+            let regExp = new RegExp(/^[A-ZА-Я]{1}$/);
+            if (!regExp.test(input.value)) {
+                alert("Корпус задается одной заглавной буквой!");
+                return false;
+            }
+        }
+    }
+
+    return false;
 }
