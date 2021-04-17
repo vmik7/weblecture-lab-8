7ea5313f3a15b1ecb3d014d4eddafae5a06ed654
vmik7 <vmik7@yandex.ru>
2021-04-17 16:18:48 +0300

    Отправка записи на добавление в БД

--- add.php
+++ add.php
@@ -1,7 +1,24 @@
 <?php include 'php/db.php' ?>
+<?php include 'php/get_colums.php' ?>
 
 <?php 
-    
+    if (isset($_GET["table"]) && $_GET["table"] == "Преподаватели") {
+        $table = "Преподаватели";
+        $title = "Новый преподаватель";
+        $columns = $prep_columns;
+
+        $options_html = "";
+
+        $ans = db_query("SELECT `Название` FROM `Кафедры`");   
+        foreach ($ans as $item) {
+            $options_html .= '<option value="' . $item["Название"] . '">' . $item["Название"] . '</option>';
+        }
+    }
+    else {
+        $table = "Кафедры";
+        $title = "Новая кафедра";
+        $columns = $kaf_columns;
+    }
 ?>
 
 
@@ -19,12 +36,30 @@
 <body class="page add-page">
     <main class="content">
         <div class="page__container add-page__container">
-            <h1 class="add-page__title">Добавить кафедру</h1>
-            <form action="index.php" method="GET" class="form">
-                <input type="text" class="form__input" placeholder="Название">
-                <input type="text" class="form__input" placeholder="Корпус">
-                <input type="text" class="form__input" placeholder="Аудитория">
-                <input type="text" class="form__input" placeholder="Количество дисциплин">
+            <h1 class="add-page__title"><?php echo $title ?></h1>
+            <form action="index.php" method="POST" class="form form_action_add-row">
+
+                <input type="text" name="table" value="<?php echo $table ?>" class="visually-hidden">
+
+                <?php 
+                    if ($table == "Кафедры") {
+                        echo '
+                            <input type="text" name="' . $columns[0]["input_name"] . '" class="input" placeholder="' . $columns[0]["title"] . '" required>
+                            <input type="text" name="' . $columns[1]["input_name"] . '" class="input input_validate_char" placeholder="' . $columns[1]["title"] . '" required>
+                            <input type="text" name="' . $columns[2]["input_name"] . '" class="input" placeholder="' . $columns[2]["title"] . '" required>
+                            <input type="number" name="' . $columns[3]["input_name"] . '" class="input input_validate_posnum" placeholder="' . $columns[3]["title"] . '" required>
+                        ';
+                    }
+                    else {
+                        echo '
+                            <input type="text" name="' . $columns[0]["input_name"] . '" class="input" placeholder="' . $columns[0]["title"] . '" required>
+                            <select name="' . $columns[1]["input_name"] . '" required>' . $options_html . '</select>
+                            <input type="text" name="' . $columns[2]["input_name"] . '" class="input" placeholder="' . $columns[2]["title"] . '" required>
+                            <input type="text" name="' . $columns[3]["input_name"] . '" class="input input_validate_tel" placeholder="' . $columns[3]["title"] . '" required>
+                        ';
+                    }
+                ?>
+
                 <button type="submit" class="button">Добавить</button>
             </form>
         </div>
